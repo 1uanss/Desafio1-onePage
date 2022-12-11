c9f5cf77937b212bc7748ffed2b4777bd5931376
1uanss <sousasluan212@gmail.com>
2022-12-11 20:51:16 -0300

    segunda parte

--- reactapp/src/pages/Components/cards/card.jsx
+++ reactapp/src/pages/Components/cards/card.jsx
@@ -1,11 +1,17 @@
 import { useState } from 'react'
 import './style.css';
 
-function Card(){
+function Card(props){
+    const {backgroundColor, color} = props
+    console.log(props)
+    const myStyleCard = {
+        backgroundColor,
+        color,
+    };
     return (
         <div className="card">
-            <div className="car-color"></div>
-            <h2 className='card-title'></h2>
+            <div style={myStyleCard} className="car-color" >.</div>
+            <h2 className='card-title'> title</h2>
             <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis est ea quisquam qui doloribus, </p>
         </div>
     )
--- reactapp/src/pages/main/MainHtml.jsx
+++ reactapp/src/pages/main/MainHtml.jsx
@@ -1,12 +1,24 @@
-import { useState } from 'react'
-import Card from '../Components/cards/card';
+import { useState} from 'react'
 import './style.css';
+import Card from '../Components/cards/card';
 
-function MainInHtml(){
+function MainInHtml() {
+    const [colors] = useState([
+        {backgroundColor: "#FF6363", color: "#FF6363"},
+        {backgroundColor: "#63ECFF", color: "#63ECFF"},
+        {backgroundColor: "#F363FF", color: "#F363FF"},
+        {backgroundColor: "#63FF73", color: "#63FF73"},
+        {backgroundColor: "#FFDD63", color: "#FFDD63"},
+        {backgroundColor: "#6663FF", color: "#6663FF"},
+    ]);
+    
     return (
-        <main className='container-2'>
-            <Card/>
+        <main className="container-2">
+            {
+                colors.map((style, idx) => <Card key={idx} backgroundColor={style.backgroundColor} color={style.color}/>)
+            }
         </main>
     )
 }
+
 export default MainInHtml;
